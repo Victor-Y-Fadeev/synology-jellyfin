efeadb57ee77097c29e393e504982e24e1ee75e2
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-02-18 23:08:03 +0300

    Complete search func

--- import_extra_files.sh
+++ import_extra_files.sh
@@ -28,61 +28,39 @@ function dictionary_to_commentary {
             local prefix="$(sed -e 'N;s|^\(.*/\).*\n\1.*$|\1\n\1|;D' <<< "$value")"
             local postfix="$(sed -e 'N;s|^.*\(/.*\)\n.*\1$|\1\n\1|;D' <<< "$value")"
 
-            # echo $prefix $postfix
-            local prefix_length="${#prefix}"
-            local postfix_length="${#postfix}"
             prefix="${#prefix}"
             postfix="${#postfix}"
-            # echo $prefix_length $postfix_length
 
             while read -r line; do
                 if (( prefix + postfix < ${#line} )); then
-                    output[$line]="${line:prefix:-postfix}"
+                    output[$line]=".${line:prefix:-postfix}"
                 fi
-
-                # if (( prefix_length + postfix_length < ${#line} )); then
-                #     to[$line]="${line:prefix_length:-postfix_length}"
-                # fi
-
-
-                # local comment="${line#"$prefix"}"
-                # comment="${comment%"$postfix"}"
-                # comment="${comment//// }"
-                # to[$line]="$comment"
-
-                # echo "'${line:$prefix_length}'"
-                # if (( prefix_length + postfix_length < ${#line} )); then
-                # echo "'${line:$prefix_length:-$postfix_length}'"
-                # else
-                # echo ''
-                # fi
             done <<< "$value"
         fi
     done
 }
 
 
-# # function move_file {
-#     local source_path="$1"
-#     local destination_path="$2"
-    
-# # }
-
-
 function import_file {
+    local source_path="$1"
+    local destination_path="$2"
+
+    echo
+    echo "'$source_path' -> '$destination_path'"
+}
+
+
+function search_extra_files {
     local source_path="$1"
     local source_folder="$(dirname "$source_path")"
     local source_file="$(basename "$source_path")"
-    local source_escape_file="$(printf '%q' "$source_file")"
-    local source_escape_name="${source_escape_file%.*}"
+    local source_name="${source_file%.*}"
 
-    local destination_path="$2"
-    local destination_folder="$(dirname "$destination_path")"
-    local destination_filename="$(basename "$destination_path")"
-    local destination_name="${destination_filename%.*}"
+    local source_file_escape="$(printf '%q' "$source_file")"
+    local source_name_escape="$(printf '%q' "$source_name")"
 
-    readarray -d '' array < <(find "$source_folder" -type f -name "${source_escape_name}.*" \
-                                ! -name "$source_escape_file" -print0)
+    readarray -d '' array < <(find "$source_folder" -type f \
+        -name "${source_name_escape}.*" ! -name "$source_file_escape" -print0)
 
     local -A dictionary
     for file in "${array[@]}"; do
@@ -92,22 +70,31 @@ function import_file {
     local -A commentary
     dictionary_to_commentary dictionary commentary
 
-    for file in "${array[@]}"; do
-        echo "'$file' -> '${commentary[$file]}'"
-    done
+    local destination_path="$2"
+    # local destination_folder="$(dirname "$destination_path")"
+    # local destination_filename="$(basename "$destination_path")"
+    # local destination_name="${destination_filename%.*}"
+    local destination_absolute="${destination_path%.*}"
 
-    # for key in "${!dictionary[@]}"; do
-    #     echo "$key -> ${dictionary[$key]}"
-    # done
+    for file in "${array[@]}"; do
+        local extension="${file##*"$source_name"}"
+        local suffix="${commentary[$file]}"
+
+        # if [[ -n "$suffix" ]]; then
+            import_file "$file" "${destination_absolute}${suffix}${extension}"
+        # else
+        #     import_file "$file" "${destination_absolute}${extension}"
+        # fi
+    done
 }
 
-import_file "$(realpath "${1}")" "$(realpath "${2}")"
+search_extra_files "$(realpath "$1")" "$(realpath "$2")"
 
 
 case "$sonarr_eventtype" in
     "Download")
         if [[ ! -z "$sonarr_isupgrade" ]] ; then
-            import_file "$sonarr_episodefile_sourcepath" "$sonarr_episodefile_path"
+            search_extra_files "$sonarr_episodefile_sourcepath" "$sonarr_episodefile_path"
         fi
         ;;
     "Rename")
