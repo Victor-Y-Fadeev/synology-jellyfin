d9c5fcb73db9e1f83d944769361fdbfe64793bd6
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-03-30 21:44:36 +0300

    Clean Up

--- fix.ps1
+++
@@ -1,6 +0,0 @@
-$adapters = Get-NetAdapterLso -IncludeHidden
-
-foreach ($adapter in $adapters) {
-	Write-Host "Disabling LSO for adapter: $($adapter.Name)"
-	Disable-NetAdapterLso -IncludeHidden -Name $adapter.Name -Confirm:$false
-}
