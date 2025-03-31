8659a83a3166102e1cc4b89a6fea6a292b3d8e86
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-03-31 06:17:24 +0300

    Try to speed up

--- scripts/load_file_structure.sh
+++ scripts/load_file_structure.sh
@@ -65,5 +65,73 @@ function recursive_json_processing {
 }
 
 
-parse_input_arguments "$@"
-recursive_json_processing "$JSON" "$DEST"
+# parse_input_arguments "$@"
+# recursive_json_processing "$JSON" "$DEST"
+
+# echo '{"path":["a", "b"]}' | jq 'walk(if type == "object" then with_entries(.key as $key | .value |= $key + .) else . end)'
+
+# echo '{"path":["a", "b"]}' | jq 'recurse(if type == "object" then keys else . end, .|type == "array")'
+# jq 'recurse(. * .; . < 20)'
+# echo '[["a", "b"],3]' | jq 'recurse(if type == "array" then . else false end)'
+
+
+# echo '["path",["a", "b", [1,2,5,3]]]' | jq 'walk(if type == "array" then .[] else . end)'
+# echo '["path",["a", "b", [1,2,5,3]]]' | jq 'flatten'
+
+
+# echo '{"path":["a", "b", {"1":["4","5"]}]}' | jq 'walk(if type == "object" then .[] else . end)'
+# echo '{"path":["a", "b", {"1":["4","5"]}], "f": {"g":"4"}}' \
+#     | jq 'walk(if type != "object" then .
+#                else with_entries(.key as $key |
+#                                  .value |= if type == "object" then with_entries(.key |= $key + .)
+#                                            elif type == "string" then $key + . else . end) | .[] end)'
+
+            #    .key as $key | .value | if type == "object" then with_entries(.key |= $key + .)
+
+# echo '{"path":["a", "b", {"1":["4","5"]}], "f": {"g":"4"}}' \
+#     | jq 'walk(if type != "object" then .
+#                else with_entries(.key as $key |
+#                                  .value |= if type == "object" then with_entries(.key |= $key + .)
+#                                            elif type == "array" then map([{"key": $key, "value": .}] | from_entries) else $key + . end) | .[] end)'
+
+
+# echo '{"path":["a", "b", {"1":["4","5"]}], "f": {"g":"4"}}' \
+#     | jq 'walk(if type == "object" then .[] else . end)'
+
+
+# echo '{"path":["a", "b", {"1":["4","5"]}], "f": {"g":"4"}}' \
+#     | jq 'walk(if type != "object" then .
+#                else with_entries(.key as $key |
+#                                  .value |= if type == "array" then map([{"key": $key, "value": .}] | from_entries) else . end) end)'
+
+# echo "{"a":["3"], "b":"4"}" | jq 'recurse(if type == "object" then .[] else . end, type != "object")'
+
+
+# echo '{"path":["a", "b", {"1":["4","5"]}], "f": {"g":"4"}}' \
+#     | jq 'walk(if type != "object" then .
+#                else to_entries | .[] | .key as $key | .value
+#                     | if type == "array" then map([{"key": $key, "value": .}] | from_entries) | .[]
+#                       else [{"key": $key, "value": .}] | from_entries end end)'
+
+
+# echo '{"path":["a", "b", {"1":["4","5"]}], "f": {"g":"4"}}' \
+#     | jq 'walk(if type != "object" then .
+#                else to_entries | .[] | .key as $key | .value
+#                     | if type == "array" then .[] else . end
+#                     | [{"key": $key, "value": .}] | from_entries end
+#                | if type != "object" then .
+#                  else to_entries | .[] | .key as $key | .value
+#                       | if type != "object" then $key + "/" + .
+#                         else with_entries(.key |= $key + "/" + .) end end)'
+
+
+
+echo '[{"a":{".":["1", "2"], "b":["3", "4"]}, "c":{".":["5"]}}, {"path":["a", "b", {"1":["4","5"]}], "f": {"g":"4"}}]' \
+    | jq 'walk(if type != "object" then .
+               else to_entries | map(.key as $key | .value
+                    | if type == "array" then .[] else . end
+                    | [{"key": $key, "value": .}] | from_entries) end)'
+            #    walk(if type != "object" then .
+            #      else to_entries | .[]|.key as $key | .value
+            #           | if type != "object" then $key + "/" + .
+            #             else with_entries(.key |= $key + "/" + .) end end)'
