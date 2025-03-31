53d2cb7c44ba56fc012d4640870692db435dca6d
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-03-31 04:02:24 +0300

    Complete load script

--- scripts/load_file_structure.sh
+++ scripts/load_file_structure.sh
@@ -24,7 +24,6 @@ function recursive_json_processing {
     local destination="$2"
     local type="$(echo "$json" | jq -r 'type')"
     local i
-    # echo "'$json'"
 
     case "$type" in
         "object")
@@ -32,10 +31,9 @@ function recursive_json_processing {
             local length="$(echo "$keys" | jq 'length')"
 
             for (( i = 0; i < length; ++i )); do
-                # echo "'$type' '$i'"
                 local key="$(echo "$keys" | jq -r --argjson index "$i" '.[$index]')"
                 local value="$(echo "$json" | jq --arg key "$key" '.[$key]')"
-                # echo "key='$key', value='$value'"
+
                 recursive_json_processing "$value" "$destination/$key"
             done
             ;;
@@ -44,8 +42,8 @@ function recursive_json_processing {
             local length="$(echo "$json" | jq 'length')"
 
             for (( i = 0; i < length; ++i )); do
-                # echo "'$type' '$i'"
                 local value="$(echo "$json" | jq --argjson index "$i" '.[$index]')"
+
                 recursive_json_processing "$value" "$destination"
             done
             ;;
@@ -53,87 +51,19 @@ function recursive_json_processing {
         "string")
             local value="$(echo "$json" | jq -r)"
             local absolute="$destination/$value"
-            local folder="$(dirname "$absolute")"
+            absolute="${absolute//"/./"/"/"}"
+            absolute="${absolute//"//"/"/"}"
 
-            # mkdir --parents "$folder"
-            # echo "$absolute" > "$absolute"
-            echo "$absolute"
+            local folder="$(dirname "$absolute")"
+            local file="${absolute#"$DEST"}"
+
+            mkdir --parents "$folder"
+            echo "$file" > "$absolute"
+            echo "$file"
             ;;
     esac
 }
 
 
-# jq 'map(type)' "$SRC"
-# echo "$JSON" | jq 'if type == "object" then "object" elif type == "array" then "array" end'
-# echo "{}" | jq 'type'
-# echo "[]" | jq 'type'
-# echo '""' | jq 'type'
-# echo '' | jq 'type'
-# echo '""' | jq '.'
-
-
-# echo "'$SRC'"
-
-# echo "'$DEST'"
-
-
-
-# parse_input_arguments "$@"
-# recursive_json_processing "$JSON" "$DEST"
-
-
-# echo '{"path":["1", "2"], "path2":[]}' | jq 'keys' | jq 'length'
-# echo '["a", "b"]' | jq --arg index 0 '.[$index | tonumber]'
-
-
-# length="3"
-# for (( i = 0; i < length; ++i )); do
-# # for ((i = 0; i < 2; i++)); do
-#     echo "$i"
-# done
-
-recursive_json_processing '{"pa th":["a", "b"], "snd":["c", "d"]}' "."
-
-
-
-
-# function B {
-#     # local json="$1"
-#     local type="$1"
-
-#     if [[ "$type" == "object" ]]; then
-#             local length=2
-#             local i
-#             for (( i = 0; i < length; ++i )); do
-#                 echo "object" $i
-#                 B "array"
-#             done
-
-#     elif [[ "$type" == "array" ]]; then
-#             local length2=3
-#             local i
-
-#             for (( i = 0; i < length2; ++i )); do
-#                 echo "array" $i
-#             done
-#     fi
-
-# }
-
-# B object
-
-# function A() {
-#     local iter=$1
-#     local length=$2
-
-#     case "object" in
-#         "object")
-#             for (( i = 0; i < length; ++i )); do
-#                 echo $iter $i
-#                 A $((iter + 1)) $i
-#             done
-#             ;;
-#     esac
-# }
-
-# A 1 3
+parse_input_arguments "$@"
+recursive_json_processing "$JSON" "$DEST"
