0acd57a1a2844754fd3e71a14c8a20f8733426a0
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-03-31 03:15:47 +0300

    Save commit for working load

--- scripts/load_file_structure.sh
+++ scripts/load_file_structure.sh
@@ -22,19 +22,34 @@ function parse_input_arguments {
 function recursive_json_processing {
     local json="$1"
     local destination="$2"
-
     local type="$(echo "$json" | jq -r 'type')"
+    local i
+    # echo "'$json'"
+
     case "$type" in
         "object")
-            echo "$type"
+            local keys="$(echo "$json" | jq 'keys')"
+            local length="$(echo "$keys" | jq 'length')"
+
+            for (( i = 0; i < length; ++i )); do
+                # echo "'$type' '$i'"
+                local key="$(echo "$keys" | jq -r --argjson index "$i" '.[$index]')"
+                local value="$(echo "$json" | jq --arg key "$key" '.[$key]')"
+                # echo "key='$key', value='$value'"
+                recursive_json_processing "$value" "$destination/$key"
+            done
             ;;
+
         "array")
             local length="$(echo "$json" | jq 'length')"
+
             for (( i = 0; i < length; ++i )); do
-                local value="$(echo "$json" | jq --arg index "$i" '.[$index | tonumber]')"
+                # echo "'$type' '$i'"
+                local value="$(echo "$json" | jq --argjson index "$i" '.[$index]')"
                 recursive_json_processing "$value" "$destination"
             done
             ;;
+
         "string")
             local value="$(echo "$json" | jq -r)"
             local absolute="$destination/$value"
@@ -71,9 +86,54 @@ function recursive_json_processing {
 # echo '["a", "b"]' | jq --arg index 0 '.[$index | tonumber]'
 
 
-#     for (( i = 0; i < length; ++i )); do
-# for ((i = 0; i < 2; i++)); do
+# length="3"
+# for (( i = 0; i < length; ++i )); do
+# # for ((i = 0; i < 2; i++)); do
 #     echo "$i"
 # done
 
-recursive_json_processing '["a", "b"]' "."
+recursive_json_processing '{"pa th":["a", "b"], "snd":["c", "d"]}' "."
+
+
+
+
+# function B {
+#     # local json="$1"
+#     local type="$1"
+
+#     if [[ "$type" == "object" ]]; then
+#             local length=2
+#             local i
+#             for (( i = 0; i < length; ++i )); do
+#                 echo "object" $i
+#                 B "array"
+#             done
+
+#     elif [[ "$type" == "array" ]]; then
+#             local length2=3
+#             local i
+
+#             for (( i = 0; i < length2; ++i )); do
+#                 echo "array" $i
+#             done
+#     fi
+
+# }
+
+# B object
+
+# function A() {
+#     local iter=$1
+#     local length=$2
+
+#     case "object" in
+#         "object")
+#             for (( i = 0; i < length; ++i )); do
+#                 echo $iter $i
+#                 A $((iter + 1)) $i
+#             done
+#             ;;
+#     esac
+# }
+
+# A 1 3
