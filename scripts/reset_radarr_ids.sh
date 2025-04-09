db073b0154832fb02c693d2c95c5b50b5c692f81
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-04-10 01:26:40 +0300

    Add Radarr IDs reset script

---
+++ scripts/reset_radarr_ids.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+DB="$DIR/../config/radarr/radarr.db"
+
+if ! dpkg --status sqlite3 &>/dev/null; then
+    sudo apt --yes install sqlite3
+fi
+
+sqlite3 "$DB" "UPDATE sqlite_sequence
+               SET seq = 0
+               WHERE name IN ('MovieFiles', 'Movies');"
+
+sqlite3 "$DB" "SELECT name, seq
+               FROM sqlite_sequence
+               WHERE name IN ('MovieFiles', 'Movies');"
