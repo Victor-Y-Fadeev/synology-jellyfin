db816285cc765af362a4565a925345b42d71732b
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-04-17 00:14:29 +0300

    Add filter for hardlinks search

--- scripts/search_hardlinks.sh
+++ scripts/search_hardlinks.sh
@@ -7,13 +7,19 @@ function parse_input_arguments {
     DIR="$(dirname "${BASH_SOURCE[0]}")"
     DIR="$(realpath "${DIR}")"
 
-    ROOT="$(realpath "${2:-"${DIR}"}")/"
-    if [[ ! -f "$1" || ! "${ROOT}" =~ "/data/" ]]; then
+    if [[ -d "$2" ]]; then
+        FILTER="$(realpath "$2")"
+        ROOT="${FILTER%%/data/*}"
+        FILTER="${FILTER#"${ROOT}"}"
+    else
+        ROOT="${DIR%%/data/*}"
+    fi
+
+    if [[ ! -f "$1" || ! -d "${ROOT}/data" ]]; then
         exit
     fi
 
     SRC="$(realpath "$1")"
-    ROOT="${ROOT%%/data/*}"
     DEST="${DIR}/hardlink_$(basename "${SRC}")"
 
     READER="${DIR}/json_to_paths.jq"
@@ -23,14 +29,16 @@ function parse_input_arguments {
 
 function search_hardlinks {
     while read -r destination; do
-        local file="${ROOT}/${destination#/}"
-        local escape="$(printf '%q' "$file")"
+        if [[ "$destination" =~ ^"$FILTER" ]]; then
+            local file="${ROOT}/${destination#/}"
+            local escape="$(printf '%q' "$file")"
 
-        while read -r source; do
-            if [[ -n "$source" ]]; then
-                echo "${source#"${ROOT}"}|${destination}"
-            fi
-        done <<< "$(find "$ROOT" -samefile "$file" ! -path "$escape" 2>/dev/null)"
+            while read -r source; do
+                if [[ -n "$source" ]]; then
+                    echo "${source#"${ROOT}"}|${destination}"
+                fi
+            done <<< "$(find "$ROOT" -samefile "$file" ! -path "$escape" 2>/dev/null)"
+        fi
     done <<< "$(cat)"
 }
 
