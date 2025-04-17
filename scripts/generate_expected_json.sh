23ae89198a1e6ebf38076c209be28a5e19055c63
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-04-18 01:21:37 +0300

    Create expected.json generator

---
+++ scripts/generate_expected_json.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+DIR="$(realpath "$DIR")"
+
+TO_KV="${DIR}/json_to_kv.jq"
+FROM_KV="${DIR}/json_from_kv.jq"
+FROM_PATHS="${DIR}/json_from_paths.jq"
+
+TESTS="$(realpath "${DIR}/../tests/cases")"
+SRC="$(realpath "${1:-.}")"
+
+
+if [[ -f "$SRC" ]]; then
+    TMP="$(mktemp -d)"
+    unzip "$SRC" -d "$TMP"
+    SRC="$TMP"
+fi
+
+
+for test in "${TESTS}"/*/; do
+    name="$(basename "$test")"
+    file="${SRC}/${name}.json"
+
+    if [[ -f "$file" ]]; then
+        mv "$file" "${test}/download/expected.json"
+
+        list="$("$TO_KV" "$file" | sed --posix --regexp-extended 's/^[^\|]*\|//' | sort | uniq)"
+        dict="$(echo "$list" | sed --posix --regexp-extended \
+            --expression 's/.*/\0|\0/' \
+            --expression 's/ 0+/ /' \
+            --expression 's/s[0-9]{2}e[0-9]{2}/\0 -/' \
+            --expression 's/\([0-9]{4}\)\./- \0/' \
+            --expression 's/([^\|]*)\|(.*)/\2|\1/')"
+
+        "$FROM_PATHS" <<< "$list" > "${test}/delete/expected.json"
+        "$FROM_KV" <<< "$dict" > "${test}/rename/expected.json"
+    fi
+done
+
+
+rm --force --recursive "$TMP"
