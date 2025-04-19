d987ef4fade730d4ce074454a0f6c80097cb7a77
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-04-19 18:31:18 +0300

    Implement export/unset of environment variables

--- tests/helpers/mock_servarr_event.bash
+++ tests/helpers/mock_servarr_event.bash
@@ -3,3 +3,35 @@
 
 SOURCE="$(realpath "${BASH_SOURCE[0]}")"
 ROOT="$(dirname "${SOURCE}")/../.."
+
+
+unset_environment_variables() {
+    local servarr="$(printenv | grep -i "arr_eventtype=" | sed 's/_.*//' | paste -sd '|')"
+
+    while read -r line; do
+        if [[ "$line" =~ ^(${servarr})_ ]]; then
+            local variable="${line%%=*}"
+            unset "$variable"
+        fi
+    done <<< "$(printenv)"
+}
+
+
+export_environment_variables() {
+    local input="${1:-"$(cat)"}"
+    if [[ -f "$input" ]]; then
+        input="$(cat "$input")"
+    fi
+
+    local keys="$(jq --raw-output 'keys[]' <<< "$input")"
+    while read -r key; do
+        local value="$(jq --raw-output --arg key "$key" '.$key' <<< "$input")"
+        export "$key"="$value"
+    done <<< "$keys"
+}
+
+
+unset_environment_variables
+export_environment_variables "$1"
+# export_environment_variables "../cases/tvdbid-72070/download/events/20250408_204316_743459087_Download.json"
+# export_environment_variables "../cases/tmdbid-18491/download/events/20250410_015320_350277224_Download.json"
