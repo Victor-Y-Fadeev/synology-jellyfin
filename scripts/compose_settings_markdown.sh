d1e80aceea054c1243135176fcfbe7dea6470c43
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-04-29 22:04:10 +0300

    Add script to generate settings markdown from Docker Compose services

---
+++ scripts/compose_settings_markdown.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+DIR="$(realpath "$DIR")"
+
+DOCKER_COMPOSE="${DIR}/../compose.yaml"
+SETTINGS_DIR="${DIR}/../docs"
+SETTINGS_FILE="${SETTINGS_DIR}/settings.md"
+
+
+if ! command -v yq &>/dev/null; then
+    sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq
+    sudo chmod +x /usr/bin/yq
+fi
+
+rm --force "$SETTINGS_FILE"
+
+for i in $(yq --output-format tsv '.services | keys' "$DOCKER_COMPOSE"); do
+    markdown="${SETTINGS_DIR}/${i}.md"
+
+    if [[ -f "$markdown" ]]; then
+        cat "$markdown" >> "$SETTINGS_FILE"
+        echo "" >> "$SETTINGS_FILE"
+    fi
+done
