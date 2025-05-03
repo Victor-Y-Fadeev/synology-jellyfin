f7291a5fd3abc4edffed649af1baed1ac47f29d1
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-05-03 22:04:30 +0300

    Support downloading of file parts

--- scripts/support_extra_files2.sh
+++ scripts/support_extra_files2.sh
@@ -27,13 +27,43 @@ function buffer_download_event {
 }
 
 
-buffer_download_event "$SONARR_BUFFER" \
+function complex_download_event {
+    local buffer="$1"
+    local instance="$2"
+    local destination="$3"
+    local source="$4"
+
+    local all
+    readarray all <<< "$(buffer_download_event "$buffer" "$instance" "$destination" "$source")"
+
+    local i
+    for (( i = 0; i < ${#all[@]}; ++i )); do
+        local current="${all[$i]%$'\n'}"
+        local extension="${current##*.}"
+
+        if (( ${#all[@]} > 1 )); then
+            extension="pt$(( i + 1 )).${extension}"
+        fi
+
+        local base="${destination%.*}"
+        local full="${base}.${extension}"
+
+        if [[ "$current" == "$source" && "$full" != "$destination" ]]; then
+            # mv --force "$destination" "$full"
+        fi
+
+        # import_extra_files "$current" "$full"
+    done
+}
+
+
+complex_download_event "$SONARR_BUFFER" \
     "/data/series/anime/Neon Genesis Evangelion (1995) [tvdbid-70350]" \
     "/data/series/anime/Neon Genesis Evangelion (1995) [tvdbid-70350]/Specials/s00e02 Neon Genesis Evangelion - The End of Evangelion.mkv" \
     "/data/downloads/series/anime/Neon Genesis Evangelion + The End of Evangelion/25. (The End of Evangelion) 01. AIR (Воздух).mkv"
 
 
-buffer_download_event "$SONARR_BUFFER" \
+complex_download_event "$SONARR_BUFFER" \
     "/data/series/anime/Neon Genesis Evangelion (1995) [tvdbid-70350]" \
     "/data/series/anime/Neon Genesis Evangelion (1995) [tvdbid-70350]/Specials/s00e02 Neon Genesis Evangelion - The End of Evangelion.mkv" \
     "/data/downloads/series/anime/Neon Genesis Evangelion + The End of Evangelion/26. (The End of Evangelion) 02. My Pure Heart For You (Искренне Ваш...).mkv"
