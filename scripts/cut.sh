2d5a0b0f9a2b98f81e440b006221f7089079e528
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-09-06 02:08:35 +0300

    Fast K frame search

--- scripts/cut.sh
+++ scripts/cut.sh
@@ -19,76 +19,36 @@ if [[ ! -f "$FRAMES" ]]; then
     cat "$ENTRIES" | jq --raw-output '.frames[] | select(.pict_type == "I") | .pts_time' > "$FRAMES"
 fi
 
-# cat "$FRAMES"
+# cat "$ENTRIES" | jq --null-input --raw-output --stream 'first(fromstream(2 | truncate_stream(inputs)) | select(.pict_type=="I") | .pts_time)'
 
 
-# cat "$ENTRIES" | jq --raw-output --stream 'limit(1;select(.[0][0] == "frames" and (.[1].pict_type == "I")) | .[1].pts_time)'
-# cat "$ENTRIES" | jq --raw-output --stream 'select(.[0][0] == "frames" and (.[1].pict_type == "I")) | .[1].pts_time'
-# cat "$ENTRIES" | jq --raw-output --stream 'fromstream(2|truncate_stream(inputs)) | select(.pict_type=="I") | .pts_time'
+function next_key_frame {
+    local input="$(realpath "${1}")"
+    local time="$(date --date "1970-01-01T${2}Z" +%s.%N)"
 
-# echo '
-#   {
-#     "results":[
-#       {
-#         "columns": ["n"],
-#         "data": [    
-#           {"row": [{"key1": "row1", "key2": "row1"}], "meta": [{"key": "value"}]},
-#           {"row": [{"key1": "row2", "key2": "row2"}], "meta": [{"key": "value"}]}
-#         ]
-#       }
-#     ],
-#     "errors": []
-#   }
-# ' | jq --stream '.'
-#   fromstream(1|truncate_stream(
-#     inputs | select(
-#       .[0][0] == "results" and 
-#       .[0][2] == "data" and 
-#       .[0][4] == "row"
-#     ) | del(.[0][0:5])
-#   ))'
+    ffprobe -loglevel quiet -select_streams v:0 -show_entries frame=pts_time,pict_type -print_format json \
+            -read_intervals "${time}%" "${input}" \
+        | jq --null-input --raw-output --stream --arg time "$time" '
+            first(fromstream(2 | truncate_stream(inputs))
+            | select(.pict_type == "I")
+            | .pts_time
+            | select(tonumber >= ($time | tonumber)))'
+}
 
+function next_key_packet {
+    local input="$(realpath "${1}")"
+    local time="$(date --date "1970-01-01T${2}Z" +%s.%N)"
 
-# cat "$ENTRIES" | jq --raw-output --stream '
-#   fromstream(1|truncate_stream(
-#     inputs
-#     | select(.[0][0]=="frames")
-#     | del(.[0][0:2])
-#   ))
-#   | select(.pict_type=="I")
-#   | .pts_time
-# '
+    ffprobe -loglevel quiet -select_streams v:0 -show_entries packet=pts_time,flags -print_format json \
+            -read_intervals "${time}%" "${input}" \
+        | jq --null-input --raw-output --stream --arg time "$time" '
+            first(fromstream(2 | truncate_stream(inputs))
+            | select(.flags | contains("K"))
+            | .pts_time
+            | select(tonumber >= ($time | tonumber)))'
+}
 
 
-# ffprobe -loglevel quiet -select_streams v:0 -show_entries packet=pts_time,flags -print_format json "$INPUT" > "packets.json"
-# cat "packets.json" | jq --raw-output --stream '
-#     fromstream(2|truncate_stream(inputs))
-#     | select(.flags | contains("K"))
-#     | .pts_time'
+next_key_frame "$INPUT" "00:00:06.000"
+next_key_packet "$INPUT" "00:00:06.000"
 
-
-# cat "$ENTRIES" | jq --raw-output --stream 'fromstream(1 | truncate_stream(inputs |
-#     select(.[0][0] == "frames" and (.[0][2] == "pict_type" or .[0][2] == "pts_time") and .[1] != null)
-# ))'
-# cat "$ENTRIES" | jq --raw-output --stream '.'
-
-
-# cat "$ENTRIES" | jq --raw-output --stream '
-#     select(.[0][0] == "frames" and (.[0][2] == "pict_type" or .[0][2] == "pts_time") and .[1] != null)
-# '
-
-# cat "$ENTRIES" | jq --raw-output --stream 'fromstream(2 | truncate_stream(inputs |
-#     select(.[0][0] == "frames" and (.[0][2] == "pict_type" or .[0][2] == "pts_time") and .[1] != null)
-#     | del(.[0][0:4])
-# ))'
-
-# cat "packets.json" | jq --raw-output --stream '.'
-
-
-# cat "packets.json" | jq -n --raw-output --stream '
-#   fromstream(1|truncate_stream(
-#     inputs | select(
-#       .[0][0] == "packets"
-#     ) | del(.[0][0:1])
-#   ))'
-cat "$ENTRIES" | jq --null-input --raw-output --stream 'first(fromstream(2 | truncate_stream(inputs)) | select(.pict_type=="I") | .pts_time)'
