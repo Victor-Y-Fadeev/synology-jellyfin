c16b177409926c987f1c9b4442abea36c11c1287
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2025-09-06 03:20:24 +0300

    Save delta cut versions

--- scripts/cut.sh
+++ scripts/cut.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+DELTA="30"
 
 INPUT="$(realpath "$1")"
 ENTRIES="${INPUT%.*}.json"
@@ -22,22 +23,9 @@ fi
 # cat "$ENTRIES" | jq --null-input --raw-output --stream 'first(fromstream(2 | truncate_stream(inputs)) | select(.pict_type=="I") | .pts_time)'
 
 
-function next_key_frame {
-    local input="$(realpath "${1}")"
-    local time="$(date --date "1970-01-01T${2}Z" +%s.%N)"
-
-    ffprobe -loglevel quiet -select_streams v:0 -show_entries frame=pts_time,pict_type -print_format json \
-            -read_intervals "${time}%" "${input}" \
-        | jq --null-input --raw-output --stream --arg time "$time" '
-            first(fromstream(2 | truncate_stream(inputs))
-            | select(.pict_type == "I")
-            | .pts_time
-            | select(tonumber >= ($time | tonumber)))'
-}
-
 function next_key_packet {
-    local input="$(realpath "${1}")"
-    local time="$(date --date "1970-01-01T${2}Z" +%s.%N)"
+    local input="$1"
+    local time="$2"
 
     ffprobe -loglevel quiet -select_streams v:0 -show_entries packet=pts_time,flags -print_format json \
             -read_intervals "${time}%" "${input}" \
@@ -48,7 +36,43 @@ function next_key_packet {
             | select(tonumber >= ($time | tonumber)))'
 }
 
+function next_key_frame {
+    local input="$1"
+    local time="$2"
 
-next_key_frame "$INPUT" "00:00:06.000"
-next_key_packet "$INPUT" "00:00:06.000"
+    ffprobe -loglevel quiet -select_streams v:0 -show_entries frame=pts_time -skip_frame nokey -print_format json \
+            -read_intervals "${time}%" "${input}" \
+        | jq --null-input --raw-output --stream --arg time "$time" '
+            first(inputs[1] | select(type == "string" and tonumber >= ($time | tonumber)))'
+}
+
+function next_key_frame_delta {
+    local input="$1"
+    local time="$2"
+    local delta="${3:-$DELTA}"
+
+    local end="0$(bc <<< "${time} + ${delta}")"
+
+    ffprobe -loglevel quiet -select_streams v:0 -show_entries frame=pts_time -skip_frame nokey -print_format json \
+            -read_intervals "${time}%${end}" "${input}" \
+        | jq --raw-output --arg time "$time" '.frames | map(.pts_time | tonumber | select(. >= ($time | tonumber))) | min'
+}
+
+function prev_key_frame {
+    local input="$1"
+    local time="$2"
+    local delta="${3:-$DELTA}"
+
+    local start="0$(bc <<< "if (${time} < ${delta}) 0 else ${time} - ${delta}")"
+    ffprobe -loglevel quiet -select_streams v:0 -show_entries frame=pts_time -skip_frame nokey -print_format json \
+            -read_intervals "${start}%${time}" "${input}" \
+        | jq --raw-output --arg time "$time" '.frames | map(.pts_time | tonumber) | max'
+}
+
+
+# time  next_key_packet "$INPUT" "06.000"
+time  next_key_frame_delta "$INPUT" "0.6000"
+time  next_key_frame "$INPUT" "0.6000"
+# prev_key_frame "$INPUT" "10.6000"
+# prev_key_frame "$INPUT" "30.03000" "5"
 
